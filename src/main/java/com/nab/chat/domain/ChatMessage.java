4be266edb6821ff1a8163c87d9a5f9f685f74c62
frnkysitumorang <94666986+frnkysitumorang@users.noreply.github.com>
2023-03-19 16:01:31 +0700

    Add files via upload

---
+++ src/main/java/com/nab/chat/domain/ChatMessage.java
@@ -0,0 +1,32 @@
+package com.nab.chat.domain;
+
+public class ChatMessage {
+	private String name;
+	private String message;
+	private MessageType messageType;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public MessageType getMessageType() {
+		return messageType;
+	}
+
+	public void setMessageType(MessageType messageType) {
+		this.messageType = messageType;
+	}
+
+}
